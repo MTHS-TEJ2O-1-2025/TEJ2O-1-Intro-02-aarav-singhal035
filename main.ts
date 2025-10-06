4a596d8cbbcf9a35d155551d15d28d688bc5b9dc
aarav-singhal035 <aarav.singhal@stu.ocsb.ca>
2025-10-06 18:26:47 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -2,7 +2,20 @@
  *
  * Created by: Aarav Singhal
  * Created on: Sep 2025
- * This program ...
+ * This program turns on and off the LED
 */
 
-basic.showString('Hello, Aarav!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    //turns on LED
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    //turns off LED
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
